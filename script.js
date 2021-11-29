dd2d4042403e2cb12aac1452432e4dd9b9f98a12
Magnus Vahlström <Wahlstrommm@gmail.com>
2021-11-29 21:03:04 +0100

    added some code

--- script.js
+++ script.js
@@ -14,41 +14,62 @@ btn.innerText = "lägg till!";
 btn.setAttribute("id","btn");
 root.append(btn);
 
+let div = document.getElementById("div");
+
+
 var btnDelete = document.createElement("button");
 btnDelete.innerText ="Radera";
 btnDelete.setAttribute("id","btnDelete");
 
+
+
+
 btn.addEventListener("click",function(){
-    console.log(input.value);
+    //console.log(input.value);
+
     let inputText = input.value;
+
     localStorage.setItem("username",inputText);
-    
-addAction()
 
-removeItem()
-
+        removeItem()
+            if("userName" && inputText){
+                
+                addAction()
+            }
 
 });
 
 
-function addAction(){
-    
+    function addAction(){
+        
+        root.innerHTML = "";
+        let text = document.createElement("h2");
+        text.setAttribute("id","text");
+        text.innerText = input.value;
+        div.appendChild(text);
+        div.append(btnDelete);
+        btn.setAttribute("id","btn");
+        input.setAttribute("id","input")
 
-    let text = document.createElement("h2");
-    text.setAttribute("id","text");
-    text.innerHTML = input.value;
-    root.appendChild(text);
-    root.append(btnDelete);
-    
-    
-}
+        
+    }
 
 
 
 
 function removeItem(){
-    btnDelete.addEventListener("click",function(){
-        console.log(input.value);
-        text.innerHTML = "";
+            btnDelete.addEventListener("click",function(){
+            //console.log(input.value);
+            
+            div.innerHTML ="";
+            btn.setAttribute("class","btn");
+            input.setAttribute("class","input");
+            
+            root.append(btn);
+            
+            root.append(input);
+
+
+            localStorage.clear();
     })
 }
